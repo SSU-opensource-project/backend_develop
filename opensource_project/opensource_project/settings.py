bad77bc16968a701223c09987001f483ffbbd909
Ju-nic2 <ata97@naver.com>
2021-10-26 11:30:06 +0900

    add : DB setting
    Database setting is not over yet
    this is a temporary work

--- opensource_project/opensource_project/settings.py
+++ opensource_project/opensource_project/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'opensource_app.apps.OpensourceappConfig',
+    'opensource_app.apps.OpensourceAppConfig',
 ]
 
 MIDDLEWARE = [
@@ -51,7 +51,7 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'opensource_project.urls'
-
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -64,11 +64,15 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
+
         },
     },
 ]
 
 WSGI_APPLICATION = 'opensource_project.wsgi.application'
+
+# 프로젝트의 위치!
+
 
 
 # Database
@@ -76,8 +80,12 @@ WSGI_APPLICATION = 'opensource_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'cloth_db',
+        'USER': 'junic',
+        'PASSWORD': 'junic',
+        'HOST': '127.0.0.1',
+        'PORT': '5432'
     }
 }
 
